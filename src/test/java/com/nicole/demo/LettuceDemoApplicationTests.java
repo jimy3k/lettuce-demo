1f803d7b5294cab125bdd1fcc2311b9378821e38
jimy3k <jimy3k@126.com>
2022-08-19 13:52:14 +0800

    pom.xml,.yml配置文件，单元测试文件修改

--- src/test/java/com/nicole/demo/LettuceDemoApplicationTests.java
+++ src/test/java/com/nicole/demo/LettuceDemoApplicationTests.java
@@ -1,13 +1,23 @@
 package com.nicole.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 @SpringBootTest
-class LettuceDemoApplicationTests {
+public class LettuceDemoApplicationTests {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        valueOperations.set("name", "jimy3k");
+        Object name = valueOperations.get("name");
+        System.out.println(name);
     }
 
 }
