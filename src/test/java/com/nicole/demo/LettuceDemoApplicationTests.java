1cc7005e3222ee3a70017b0c14af258bcd8e2a6c
jimy3k <jimy3k@126.com>
2022-08-19 11:39:02 +0800

    Initial commit

---
+++ src/test/java/com/nicole/demo/LettuceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicole.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LettuceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
